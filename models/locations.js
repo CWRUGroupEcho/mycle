d92dbd3d49c6b11cb96179ca64e9081b3f388043
lndillon <lndillon>
2018-05-12 13:29:25 -0400

    location model

---
+++ models/locations.js
@@ -0,0 +1,30 @@
+// Dependencies
+
+
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references my connection to the DB.
+var connection = require("");
+
+// Creates a Location model that matches up with DB
+var Location = connection.define("book", {
+  name: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  address: {
+    type: Sequelize.STRING
+  },
+  photo: {
+    type: Sequelize.string
+  }
+}, {
+  timestamps: false
+});
+
+// Syncs with DB
+Location.sync();
+
+// Makes the Location Model available for other files 
+module.exports = Location;
