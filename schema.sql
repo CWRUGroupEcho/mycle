dcb554eddd5846dbad9a62c9d700fd5ff723e54f
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-05-17 14:32:32 -0400

    added seeds.sql and some data

--- schema.sql
+++ schema.sql
@@ -2,17 +2,18 @@
 
 USE mycle_db;
 
--- CREATE TABLE locations
--- (
--- 	id int NOT NULL AUTO_INCREMENT,
--- 	name varchar(50) NOT NULL,
--- 	description varchar(250) NOT NULL,
---  adress varchar(250) NOT NULL,
---  picture_url varchar(250) NOT NULL,
---  likes int DEFAULT 0,
--- 	PRIMARY KEY (id)
--- );
-/* 
+CREATE TABLE locations
+(
+	id int NOT NULL AUTO_INCREMENT,
+	locationName varchar(50) NOT NULL,
+	category varchar(50) NOT NULL,
+	description varchar(250) NOT NULL,
+ 	address varchar(250) NOT NULL,
+ 	picture_url varchar(250) NOT NULL,
+ 	likes int DEFAULT 0,
+	PRIMARY KEY (id)
+);
+ 
 CREATE TABLE reviews
 (
     id int NOT NULL AUTO_INCREMENT,
@@ -20,7 +21,7 @@ CREATE TABLE reviews
 	review varchar(250) NOT NULL,
     created_at DATE,
     PRIMARY KEY (id)
-); */
+);
 
-/* ALTER TABLE locations
-ADD COLUMN likes int DEFAULT 0 AFTER picture_url; */
+ ALTER TABLE locations
+ADD COLUMN likes int DEFAULT 0 AFTER picture_url;
