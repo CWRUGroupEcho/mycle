7160e3a443ef8e311cf6cc8b1257d7b6170efe9c
Fuzzy-chan <fuzzych4an@gmail.com>
2018-05-17 18:58:12 -0400

    saving my current work

--- seeds.sql
+++ seeds.sql
@@ -1,5 +1,7 @@
 USE mycle_db;
 
-INSERT INTO locations (name, description, adress, picture_url) VALUES("Test 2", "Another Location", "Such and Such Drive, Lakewood", "route here");
+-- INSERT INTO locations (name, description, adress, picture_url) VALUES("Test 2", "Another Location", "Such and Such Drive, Lakewood", "route here");
+
+UPDATE locations SET picture_url="/img/brasaGrill.jpg" WHERE id=1;
 
 SELECT * FROM locations;
