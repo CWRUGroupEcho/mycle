1ca3ca90a8dce8da8303c77720639b6c82226ca4
Fuzzy-chan <fuzzych4an@gmail.com>
2018-05-17 19:21:11 -0400

    updated location routes

--- routes/locations-routes.js
+++ routes/locations-routes.js
@@ -1,49 +1,141 @@
 // dependencies
+var db = require("../models")
+
+module.exports = function(app) {
+
+// welcome page get all for now - need to alter to get most liked locations
+app.get("/", function(req, res) {
+  db.Locations.findAll({
+  	order: [
+  	  ["likes", "DESC"]
+  	],
+  	limit: 3
+}).then(function(results) {
+    var hbsObject = {
+      Locations: results
+    };
+    console.log(hbsObject);
+    // res.render("welcome", hbsObject);
+    res.json(hbsObject)
+  });
+});
+
+// get by location
+app.get("/api/locations/:id", function(req, res) {
+  db.Locations.findOne({
+  	where: {
+  	 id: req.params.id
+  	},
+  	include: [db.Reviews]
+  }).then(function(results) {
+  	var hbsObject = {
+  	  Locations: results
+  	};
+  	console.log(hbsObject);
+  	// res.render("location", hbsObject);
+  	res.json(hbsObject)
+  });
+});
+
+// get by category
+app.get("/api/category/:category", function(req, res) {
+  db.Locations.findAll({
+  	where: {
+  		category: req.params.category
+  	},
+
+  	// include: [db.Reviews]
+  }).then(function(results) {
+  	var hbsObject = {
+  	  Locations: results
+  	};
+  	console.log(hbsObject);
+  	// res.render("category", hbsObject);
+  	res.json(hbsObject)
+  });
+});
+
+//get reviews for locations
+app.get("/api/reviews/:Locationid", function(req, res) {
+  db.Reviews.findAll({
+	where: {
+	  Locationid: req.params.Locationid
+	},
+	include: [db.Locations]
+  }).then(function(results) {
+  	var hbsObject = {
+  		Reviews: results
+  	};
+  	console.log(hbsObject);
+  	// res.render("reviews", hbsObject);
+  	res.json(hbsObject)
+  })
+})
+
+// put for liking locations
+app.put("/api/likes", function(req, res) {
+  var localLikes = Number(req.params.likes)
+  var newLikes = (localLikes + 1)
+  console.log("new likes: " + newLikes)
+  db.Locations.update({ 
+	likes: newLikes
+
+  }).then(function(results) {
+	  var hbsObject = {
+		Locations: results
+	  };
+	  console.log(hbsObject);
+	  res.render("location", hbsObject);
+  });
+});
 
 
-var db = require("../models");
+}
 
-module.exports = function (app) {
 
-	// welcome page get all for now - need to alter to get most liked locations
-	app.get("/", function (req, res) {
-		db.locations.findAll({}).then(function (results) {
-			var hbsObject = {
-				Location: results
-			};
-			res.render("welcome", hbsObject);
-		});
-	});
+// var db = require("../models");
 
-	// get by location
-	app.get("/api/name/:name", function (req, res) {
-		Location.findOne({
-			where: {
-				name: req.params.name
-			}
-		}).then(function (results) {
-			var hbsObject = {
-				Location: data
-			};
-			console.log(hbsObject);
-			res.render("location", hbsObject);
-		});
-	});
+// module.exports = function (app) {
 
-	// get by category
-	app.get("/api/category/:category", function (req, res) {
-		Location.findOne({
-			where: {
-				cateory: req.params.category
-			}
-		}).then(function (results) {
-			var hbsObject = {
-				Location: data
-			};
-			console.log(hbsObject);
-			res.render("category", hbsObject);
-		});
-	});
-};
+// 	// welcome page get all for now - need to alter to get most liked locations
+// 	app.get("/", function (req, res) {
+// 		db.locations.findAll({}).then(function (results) {
+// 			var hbsObject = {
+// 				Location: results
+// 			};
+// 			res.render("welcome", hbsObject);
+// 		});
+// 	});
+
+// 	// get by location
+// 	app.get("/api/name/:name", function (req, res) {
+// 		Location.findOne({
+// 			where: {
+// 				name: req.params.name
+// 			}
+// 		}).then(function (results) {
+// 			var hbsObject = {
+// 				Location: data
+// 			};
+// 			console.log(hbsObject);
+// 			res.render("location", hbsObject);
+// 		});
+// 	});
+
+// 	// get by category
+// 	app.get("/api/category/:category", function (req, res) {
+// 		Location.findOne({
+// 			where: {
+// 				cateory: req.params.category
+// 			}
+// 		}).then(function (results) {
+// 			var hbsObject = {
+// 				Location: data
+// 			};
+// 			console.log(hbsObject);
+// 			res.render("category", hbsObject);
+// 		});
+// 	});
+// };
 
 // post for liking locations
