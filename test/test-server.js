2f15a3ab04f1de3ee1f0f19d78491f684119201c
Kevin J Glasow <kevin.glasow@gmail.com>
2018-05-17 20:23:29 -0400

    Demo mocha/chai test

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,27 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server = require('../server/app');
-var should = chai.should();
+var Nightmare = require("nightmare");
+var expect = require("chai").expect;
 
-chai.use(chaiHttp);
+describe("Demo", function() {
+  // The default tests in mocha is 2 seconds.
+  // Extending it to 30 seconds to have time to load the pages
+
+  this.timeout(30000);
+  it("should open google", function(done) {
+    // entering the page url
+    Nightmare({ show: true })
+      .goto("https://www.google.com")
+      // Evaluate the title
+      .evaluate(function() {
+        return document.title;
+      })
+      // Asset the title is as expected
+      .then(function(title) {
+        expect(title).to.equal("Google");
+        done();
+      });
+  });
+
+//   it("Will throw a test error", function() {
+//     throw new Error("Failure is always an option, and that's the option I chose for this function.");
+//   });
+});
