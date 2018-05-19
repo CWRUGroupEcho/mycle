a7c774a7608d036b53e8c96d19eaf1a84570ba98
Fuzzy-chan <fuzzych4an@gmail.com>
2018-05-19 13:08:54 -0400

    omg it works!

--- public/js/main.js
+++ public/js/main.js
@@ -3,55 +3,14 @@ $(document).ready(function() {
 
 
 // PUT for liking locations
-$(".LIKEBUTTON").on("click", function(event) {
-	var id = $(this).data("id");
-	var newLikes = (Number($(this).data("likes"))+1);
-
-	var newLikeNum = {
-		likes: newLikes
-	};
-
-	// send PUT request
-	$.ajax("/api/likes/" + id, {
+$(".like").on("click", function(event) {
+	console.log("clicked")
+	
+	//send PUT request
+	$.ajax("/api/likes", {
 		type: "PUT",
-		data: newLikeNum
-	}).then(
-	function() {
-		console.log("likes updated to: " + newLikeNum);
-		// reload the page to get updated number of likes
-		location.reload();
+		data: {'id': $(this).data("id")}
 	})
-},
+});
 
-$(".SUBMITBUTTON").on("submit", function(event) {
-	event.preventDefault();
-
-	var newReview = {
-		author: $("#AUTHOR").val().trim(),
-		body: $("#BODY").val()
-	};
-
-	// send POST request
-	$.ajax("/api/review", {
-		type: "POST",
-		data: newReview
-	}).then(
-	function() {
-		console.log("new review posted");
-
-		location.reload();
-	})
-
-
-
-})
-
-
-
-
-
-
-
-
-
-}
+});
