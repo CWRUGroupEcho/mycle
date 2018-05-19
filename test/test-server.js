7de87f27b47fd57bcc9cfb45928e24a8654a8492
Kevin J Glasow <kevin.glasow@gmail.com>
2018-05-19 13:16:11 -0400

    testing working

--- test/test-server.js
+++ test/test-server.js
@@ -1,27 +1,81 @@
 var Nightmare = require("nightmare");
 var expect = require("chai").expect;
 
-describe("Demo", function() {
+describe("Homepage Test", function () {
   // The default tests in mocha is 2 seconds.
   // Extending it to 30 seconds to have time to load the pages
 
   this.timeout(30000);
-  it("should open google", function(done) {
-    // entering the page url
-    Nightmare({ show: true })
-      .goto("https://www.google.com")
+  var url = "http://localhost:8080";
+
+  // First test to see if the server is running
+
+  it("should send user to the main page", function (done) {
+    // ID for the login button.
+    Nightmare({
+        show: true
+      })
+      .goto(url)
       // Evaluate the title
-      .evaluate(function() {
+      .evaluate(function () {
         return document.title;
       })
       // Asset the title is as expected
-      .then(function(title) {
-        expect(title).to.equal("Google");
+      .then(function (title) {
+        expect(title).to.equal("MyCLE");
         done();
       });
   });
 
-//   it("Will throw a test error", function() {
-//     throw new Error("Failure is always an option, and that's the option I chose for this function.");
-//   });
+  // Second test to confirm that the restaurants link works
+
+  it("should load restaurants", function (done) {
+    new Nightmare({
+      show: true
+    })
+    .goto(url)
+    .click("#restaurants")
+    .evaluate(function () {
+      return document.title;
+    })
+    .then(function (title) {
+      expect(title).to.equal("MyCLE");
+      done();
+    });
+  });
+
+    // Second test to confirm that the Resturants link works
+
+    it("should load Nightlife", function (done) {
+      new Nightmare({
+        show: true
+      })
+      .goto(url)
+      .click("#nightlife")
+      .evaluate(function () {
+        return document.title;
+      })
+      .then(function (title) {
+        expect(title).to.equal("MyCLE");
+        done();
+      });
+    });
+
+      // Second test to confirm that the Resturants link works
+
+  it("should load Culture", function (done) {
+    new Nightmare({
+      show: true
+    })
+    .goto(url)
+    .click("#culture")
+    .evaluate(function () {
+      return document.title;
+    })
+    .then(function (title) {
+      expect(title).to.equal("MyCLE");
+      done();
+    });
+  });
+
 });
